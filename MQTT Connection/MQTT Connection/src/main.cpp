756bec7eb709f3d95157207459514a9d5510fc80
Denis-Afanasii <denis1afanasii@gmail.com>
2024-01-15 17:19:22 +0000

    kiijij

--- MQTT Connection/MQTT Connection/src/main.cpp
+++ MQTT Connection/MQTT Connection/src/main.cpp
@@ -5,19 +5,16 @@
 // #include <DallasTemperature.h>
 #include <Ultrasonic.h>
 
-
 #define ADC_VREF_mV 3300.0
 #define ADC_RESOLUTION 4096.0
-#define PIN_LM35 25
-
+#define PIN_LM35 32
 #define PELTIER 19
 
 unsigned long previousMillisPeltier = 0;
 unsigned long previousMillisTemperature = 0;
+unsigned long previousMillisDistance = 0;
 int interval = 5000;
-
 int peltierState = 0;
-
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -25,7 +22,7 @@ PubSubClient client(espClient);
 OneWire oneWire(26);
 // DallasTemperature sensors(&oneWire);
 
-const char *mqtt_broker = "192.168.253.42";
+const char *mqtt_broker = "192.168.50.10";
 const char *mqtt_username = "pwdam";
 const char *mqtt_password = "pwdam";
 const String mqtt_clientId = "esp32-client";
@@ -75,8 +72,7 @@ void setup()
   Serial.println("\nConsole started.");
 
 
-
-  WiFi.begin("pi_pwdam_grupo1", "pwdamBestGrupo");
+  WiFi.begin("RPiHotspot", "1234567890");
 
   Serial.println("A tentar conectar");
   while (WiFi.status() != WL_CONNECTED)
@@ -87,14 +83,14 @@ void setup()
   pinMode(PELTIER, OUTPUT);
 
   client.setServer(mqtt_broker, port);
-  Serial.print("Connecting");
+  Serial.println("Connecting");
   while (!client.connected())
   {
     client.connect(mqtt_clientId.c_str(), mqtt_username, mqtt_password);
 
     delay(500);
   }
-  Serial.print("Connected to MQTT");
+  Serial.println("Connected to MQTT");
   // sensors.begin();
   client.subscribe("ultraSonic");
   // client.setCallback(callback);
@@ -105,12 +101,13 @@ void setup()
 void sendDistance()
 {
   const char *topic = "distance";
-  Serial.print(ultrasonic1.read());
-  const String message = String(ultrasonic1.read());
+  String message = "{\"clientId\": \"" + WiFi.macAddress() + "\", \"message\": \"" + ultrasonic1.read() + "\"}";
 
-  Serial.println("Sending message to broker: '" + message + "'.");
+  Serial.println("Sending Distance: '" + message + "'.");
 
   client.publish(topic, String(message).c_str());
+
+//  {"clientId":"","message":""}
 }
 
 void sendMacAdress()
@@ -118,18 +115,24 @@ void sendMacAdress()
   const char *topic = "MacAdress";
   const String message = String(WiFi.macAddress());
 
-  Serial.print("Sending message to broker: '" + message + "'.");
-
+  Serial.println("Sending MacAdress: '" + message + "'.");
   client.publish(topic, String(message).c_str());
 }
 
-/*void sendTemperature(){
-  const char *topic = "Temperature";
-  const String message = String(WiFi.macAddress());
+void sendTemp()
+{
+  int adcVal = analogRead(PIN_LM35);
+  float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
 
-  Serial.print("Sending temperature to broker: '"+ message + "'.");
-  client.publish(topic, String(message).c_str);
-}*/
+  float tempC = milliVolt / 10;
+
+  const char *topic = "Temperature";
+  String message = "{\"clientId\": \"" + WiFi.macAddress() + "\", \"message\": \"" + tempC + "\"}";
+
+  Serial.println("Sending Temperature: '" + message + "'.");
+
+  client.publish(topic, String(message).c_str());
+}
 
 
 /*void sendTemp()
@@ -159,40 +162,29 @@ void loop()
   // sendTemp();
   unsigned long currentMillisPeltier = millis();
   unsigned long currentMillisTemperature = millis();
+  unsigned long currentMillisDistance = millis();
 
-  int adcVal = analogRead(PIN_LM35);
-  float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
 
-  /*if (currentMillisPeltier - previousMillisPeltier >= interval && peltierState == 0)
-  {
-    peltierState = 1;
-    previousMillisPeltier = currentMillisPeltier;
-
-    Serial.println("A ligar o Modulo durante 5 segundos");
-    digitalWrite(PELTIER, HIGH);
-    // delay(20000);
-  } else {
-    digitalWrite(PELTIER, HIGH);
-  }*/
 
   if (currentMillisTemperature - previousMillisTemperature >= 3000)
   {
-
     previousMillisTemperature = currentMillisTemperature;
 
-    float tempC = milliVolt / 10;
-    Serial.print("Temperatura: ");
-    Serial.println(tempC);
+    sendTemp();
   }
-  
-  sendDistance();
+
+  if (currentMillisDistance - previousMillisDistance >= 10000)
+  {
+    previousMillisDistance = currentMillisDistance;
+
+    sendDistance();
+  }
+
+  ;
 
   if (MacSend == 0)
   {
     sendMacAdress();
-    Serial.println("Sending Mac Adress");
     MacSend = 1;
   }
-
-  delay(1000);
 }
